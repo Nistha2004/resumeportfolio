4726f35b91f3edac7b5c89c026928bf2e276ae0d
Nistha2004 <166930274+Nistha2004@users.noreply.github.com>
2026-03-27 02:10:24 -0800

    Update index.js

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -8,17 +8,19 @@ const WorkCard = ({ img, name, description, onClick }) => {
     >
       <div
         className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
-        style={{ height: "600px" }}
+        style={{ height: "300px" }}
       >
         <img
+          src={img}
           alt={name}
           className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
-          src={img}
-        ></img>
+        />
       </div>
+
       <h1 className="mt-5 text-3xl font-medium">
         {name ? name : "Project Name"}
       </h1>
+
       <h2 className="text-xl opacity-50">
         {description ? description : "Description"}
       </h2>
